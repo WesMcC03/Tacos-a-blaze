35205205f548c59c29f75fee02bda5d5a0eee9d7
Wes McC <wkmccowin@yahoo.com>
2019-06-05 16:00:13 -0700

    mysql database and tables made, connection.js complete, fiinish orm

---
+++ db/schema.sql
@@ -0,0 +1,12 @@
+CREATE DATABASE tacos_db;
+USE tacos_db;
+
+CREATE TABLE tacos
+(
+	id int NOT NULL AUTO_INCREMENT,
+	taco_name varchar(255) NOT NULL,
+	shell varchar(255) NOT NULL,
+    vegetarian BOOLEAN DEFAULT false,
+    picked_up BOOLEAN,
+	PRIMARY KEY (id)
+);
---
+++ db/seeds.sql
@@ -0,0 +1,4 @@
+INSERT INTO tacos (taco_name, vegetarian, shell) VALUES ('Wasabi Salmon', true, 'hard flour');
+INSERT INTO tacos (taco_name, vegetarian, shell) VALUES ('Spicy Brisket', true, 'soft flour');
+INSERT INTO tacos (taco_name, vegetarian, shell) VALUES ('Veggilicious', true, 'soft flour');
+INSERT INTO tacos (taco_name, vegetarian, shell) VALUES ('Pineapple Tofu', true, 'soft flour') ;
